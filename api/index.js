330c638bffcbfc966814529b171566a013045eab
Ahmed-Albassiouni <ahmedalbassiouni0@gmail.com>
2025-06-16 23:14:49 +0300

    return server

--- api/index.js
+++ api/index.js
@@ -1,48 +1,48 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const dotenv = require("dotenv");
-require("dotenv").config();
-const bodyParser = require("body-parser");
-const path = require("path");
-const propertyRoutes = require("./routes/propertyRoutes");
-const authRoutes = require("./routes/auth");
-const bookingRoutes = require("./routes/bookingRoutes");
-const adminAuthRoutes = require("./routes/adminAuth");
-const adminRoutes = require("./routes/adminRoutes");
-const index=require("./api/index");
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const cors = require("cors");
+// const dotenv = require("dotenv");
+// require("dotenv").config();
+// const bodyParser = require("body-parser");
+// const path = require("path");
+// const propertyRoutes = require("./routes/propertyRoutes");
+// const authRoutes = require("./routes/auth");
+// const bookingRoutes = require("./routes/bookingRoutes");
+// const adminAuthRoutes = require("./routes/adminAuth");
+// const adminRoutes = require("./routes/adminRoutes");
+// const index=require("./api/index");
 
-// api/index.js
-export default function handler(req, res) {
-  res.status(200).json({ message: "Hello from API!" });
-}
+// // api/index.js
+// export default function handler(req, res) {
+//   res.status(200).json({ message: "Hello from API!" });
+// }
 
-//test
-// إعدادات عامة
-const app = express();
-app.use(express.json());      // قراءة body بصيغة JSON
-app.use(cors());  // لتفعيل الوصول من أي دومين
-app.use(bodyParser.json());
-app.use("/uploads", express.static(path.join(__dirname, "uploads"))); // إتاحة الصور علنًا
+// //test
+// // إعدادات عامة
+// const app = express();
+// app.use(express.json());      // قراءة body بصيغة JSON
+// app.use(cors());  // لتفعيل الوصول من أي دومين
+// app.use(bodyParser.json());
+// app.use("/uploads", express.static(path.join(__dirname, "uploads"))); // إتاحة الصور علنًا
 
-// الاتصال بـ MongoDB
-const url = process.env.MONGO_URI;
-mongoose.connect(url)
-.then(() => console.log("✅ Connected to MongoDB Atlas"))
-.catch(err => console.error("❌ MongoDB connection error:",err));
+// // الاتصال بـ MongoDB
+// const url = process.env.MONGO_URI;
+// mongoose.connect(url)
+// .then(() => console.log("✅ Connected to MongoDB Atlas"))
+// .catch(err => console.error("❌ MongoDB connection error:",err));
 
-// Routes
+// // Routes
 
-//الروتر الخاص ب تسجيل الدخول
-app.use("/api/auth", authRoutes);
-// ربط الروتر الخاص بالعقارات
-app.use("/api/properties", propertyRoutes);
-//الروتر الخاص ب الحجوزات
-app.use("/api/bookings", bookingRoutes);
-// ربط الراوتر الخاص بتسجيل الأدمين
-app.use("/api/admin", adminAuthRoutes); 
-//ربط الروتر الخاص ب التحكم في اليوزر
-app.use("/api/admin", adminRoutes);
+// //الروتر الخاص ب تسجيل الدخول
+// app.use("/api/auth", authRoutes);
+// // ربط الروتر الخاص بالعقارات
+// app.use("/api/properties", propertyRoutes);
+// //الروتر الخاص ب الحجوزات
+// app.use("/api/bookings", bookingRoutes);
+// // ربط الراوتر الخاص بتسجيل الأدمين
+// app.use("/api/admin", adminAuthRoutes); 
+// //ربط الروتر الخاص ب التحكم في اليوزر
+// app.use("/api/admin", adminRoutes);
 
 
 
